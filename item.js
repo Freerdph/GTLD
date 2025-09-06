9c3fba2ceabec46f0028a9bbe4dfc6b6815a466b
FASCA <cioobelummandi@gmail.com>
2025-09-07 05:11:25 +0700

    item.js

---
+++ item.js
@@ -0,0 +1,6 @@
+// Daftar item GTPS (default + custom)
+module.exports = [
+    { id: 1, name: "Dirt", type: "block" },
+    { id: 2, name: "CustomSeed", type: "seed" },
+    { id: 3, name: "MagicStone", type: "block" } // contoh item custom
+];
